ae44b87c8aeb90f772d1205671239d9dc3d2ceff
ObryanVerheyde <verheyde.obryan@gmail.com>
2017-05-04 20:19:21 +0200

    add footer

--- Javascript/default.js
+++ Javascript/default.js
@@ -1,7 +1,36 @@
 $(function (){
+ $(".onglet_equipe").hover(function(){
+   $(".sous_onglet_equipe").css({
+     "display" : "block",
+   })
+ },
+function(){
+  $(".sous_onglet_equipe").css({
+    "display" : "none",
+  })
+})
+})
+$(function (){
  $(".onglet_projet").hover(function(){
    $(".sous_onglet_projet").css({
-     "color" : "red",
+     "display" : "block",
    })
- })
+ },
+function(){
+  $(".sous_onglet_projet").css({
+    "display" : "none",
+  })
+})
+})
+$(function (){
+ $(".onglet_inscription").hover(function(){
+   $(".sous_onglet_inscription").css({
+     "display" : "block",
+   })
+ },
+function(){
+  $(".sous_onglet_inscription").css({
+    "display" : "none",
+  })
+})
 })
